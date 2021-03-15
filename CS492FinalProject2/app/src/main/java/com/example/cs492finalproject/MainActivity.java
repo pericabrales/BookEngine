17fe2fd183d8d23bb39180ac6deec0351744c067
Peri <cabralep@oregonstate.edu>
2021-03-14 17:04:09 -0700

    pre-merge commit

--- CS492FinalProject2/app/src/main/java/com/example/cs492finalproject/MainActivity.java
+++ CS492FinalProject2/app/src/main/java/com/example/cs492finalproject/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.cs492finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        this.sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        this.sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        //need a view model first
     }
 }
